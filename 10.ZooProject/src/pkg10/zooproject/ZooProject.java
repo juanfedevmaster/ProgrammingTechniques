616e410082f4c80e25aef26b3e35df2acbfe2050
Juan Fe <165521278+juanfedevmaster@users.noreply.github.com>
2024-08-30 07:45:14 -0500

    Update Zoo proyect
    Update Zoo proyect

--- 10.ZooProject/src/pkg10/zooproject/ZooProject.java
+++ 10.ZooProject/src/pkg10/zooproject/ZooProject.java
@@ -7,6 +7,8 @@ package pkg10.zooproject;
 import java.util.ArrayList;
 import java.util.Scanner;
 import pkg10.zooproject.Entites.Animal;
+import pkg10.zooproject.Entites.Enumerator.Skin;
+import pkg10.zooproject.Entites.Mammal;
 
 /**
  *
@@ -36,7 +38,34 @@ public class ZooProject {
 
             option = scan.nextInt();
             
-            
+            switch(option){
+                case 1:
+                    Mammal mammal = new Mammal();
+                    System.out.print("Insert name of mammal: ");
+                    var name = scan.next();
+                    
+                    mammal.setName(name);
+                    System.out.println("Kind of skins.");
+                    System.out.println("1. FEATHER");
+                    System.out.println("2. SCALES");
+                    System.out.println("3. FUR");
+                    System.out.print("Insert skin of mammal: ");
+                    var skin = scan.nextInt();
+                    
+                    switch (skin) {
+                        case 1:
+                            mammal.setSkin(Skin.FEATHER);
+                            break;
+                        case 2:
+                            mammal.setSkin(Skin.SCALES);
+                            break;
+                        default:
+                            mammal.setSkin(Skin.FUR);
+                            break;
+                    }
+                    
+                    lstAnimals.add(mammal);
+            }
         }
     }
 
